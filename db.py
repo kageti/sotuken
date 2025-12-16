77f9eede75a707dff007d00eb6a56e5ed5c689b6
unknown <221060>
2025-12-16 12:01:03 +0900

    DB接続

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ import os
 from mysql.connector import pooling
 
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "10.16.73.155"),
+    "host": os.getenv("DB_HOST", "10.16.73.200"),
     "port": int(os.getenv("DB_PORT", "3306")),
-    "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", ""),
+    "user": os.getenv("DB_USER", "appuser"),
+    "password": os.getenv("DB_PASSWORD", "sotuken"),
     "database": os.getenv("DB_NAME", "sotuken"),
 }
 
